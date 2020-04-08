f93db4038044160e4d848ec47ca6e20b17f1e2db
Mathias Christensen <math8045@easv365.dk>
2020-04-08 12:57:57 +0200

    contactsview er færdig for nu - mere skal implementeres senere når der laves en server

--- MVVMExercises/MVVMExercises/ViewModels/ContactsViewModel.cs
+++ MVVMExercises/MVVMExercises/ViewModels/ContactsViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
 using System.Text;
+using Xamarin.Forms;
 
 namespace MVVMExercises.ViewModels
 {
@@ -13,39 +14,37 @@ namespace MVVMExercises.ViewModels
 
         public event PropertyChangedEventHandler PropertyChanged;
 
-        public string UserName { get; set; }
-
         public ContactsViewModel()
         {
             Users = new ObservableCollection<User>(){
                 new User()
                 {
                     ID = 0,
-                    UserName = "Mathias"
+                    Username = "Mathias"
                 },
                 new User()
                 {
                     ID = 1,
-                    UserName = "test1"
+                    Username = "test1"
                 },
                 new User()
                 {
                     ID = 2,
-                    UserName = "test2"
+                    Username = "test2"
                 },
                 new User()
                 {
                     ID = 3,
-                    UserName = "test3"
+                    Username = "test3"
                 },
                 new User()
                 {
                     ID = 4,
-                    UserName = "test4"
+                    Username = "test4"
                 }
 
             };
-
+            isShownNewContact = true;
         }
 
         private ObservableCollection<User> users { get; set; }
@@ -55,6 +54,56 @@ namespace MVVMExercises.ViewModels
             get { return users; }
             set { users = value; OnPropertyChanged(); }
         }
+
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value; OnPropertyChanged(); }
+        }
+
+        private bool isShown;
+
+        public bool IsShown
+        {
+            get { return isShown; }
+            set { isShown = value; OnPropertyChanged(); }
+        }
+
+        private bool isShownNewContact;
+
+        public bool IsShownNewContact
+        {
+            get { return isShownNewContact; }
+            set { isShownNewContact = value; OnPropertyChanged(); }
+        }
+
+        private string getUser;
+
+        public string GetUser 
+        {
+            get { return getUser; }
+            set { getUser = value; OnPropertyChanged(); }
+        }
+
+        public Command CreateContactCMD => new Command(async () =>
+        {
+            
+            Users.Add( new User() { Username = GetUser });
+            Console.WriteLine(GetUser);
+            IsShownNewContact = true;
+            IsShown = false;
+        });
+
+        public Command ShowNewContactsCMD => new Command(async () =>
+        {
+            IsShown = true;
+            IsShownNewContact = false;
+            Console.WriteLine("add contact tapped!");
+            Console.WriteLine(IsShown);
+            
+        });
 
         public void OnPropertyChanged([CallerMemberName] string name = "")
         {
