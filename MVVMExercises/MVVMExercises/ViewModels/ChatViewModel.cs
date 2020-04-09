5127a36ab6d35c9d7c1fcc71b75bec51b8295e73
MartinIversen8 <martin-iversen@hotmail.com>
2020-04-09 15:41:17 +0200

    d

---
+++ MVVMExercises/MVVMExercises/ViewModels/ChatViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR.Client;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMExercises.ViewModels
+{
+    class ChatViewModel : BaseViewModel
+    {
+        private HubConnection hubConnection;
+        private string _name;
+        private string _message;
+        private ObservableCollection<MessageModel> _messages;
+        private bool _isConnected;
+        async Task Connect()
+        {
+            await hubConnection.StartAsync();
+            await hubConnection.InvokeAsync("JoinChat", Name);
+
+            IsConnected = true;
+        }
+    }
+}
